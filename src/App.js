9269e29bee7fcd5f6e1ea54b9faaf99f1f941cda
abdullah <mariyahassangaya12@gmail.com>
2024-01-17 16:22:20 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import Header from './header';
+function App() {
+  return (
+    <div>
+      <Header />
+      <Media />
+      <Media />
+    </div>
+  );
+}
+function Media() {
+  return (
+    <div className='media'>
+      <div className='media-data'>
+        {/* <video  >
+          <source src='one.mp4' type='video/mp4'></source>
+        </video> */}
+        <embed src='suffer.mp4' width={"100%"}></embed>
+        {/* <img src='dorm.png' alt='hoto' /> */}
+
+      </div>
+      <div className='medi'>
+        <div className='med'>is coding still worth in
+          2024 (s an ex-google</div>
+        <div className='med'><button>Download</button></div>
+      </div>
+      <div className='media-name'></div>
+      <div className='media-button'></div>
+    </div>
+  )
+}
+export default App;
---
+++ src/header.js
@@ -0,0 +1,10 @@
+import './App.css';
+function Header() {
+    return (
+        <div className='header'>
+            <h1><span id="media">Sunnahweb</span></h1>
+        </div>
+    );
+}
+
+export default Header;
