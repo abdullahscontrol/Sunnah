a68c600b7657108aabb5f3537962b930513f1fc0
abdullah <mariyahassangaya12@gmail.com>
2024-01-18 15:59:47 +0100

    large commit ever

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Header from './header';
+import './desktop.css';
 function App() {
   return (
-    <div>
+    <div className='thehole'>
       <Header />
       <Media />
       <Media />
+      <Media />
+      <Media />
+      <Media />
+      <Welcomebox />
     </div>
   );
 }
@@ -13,21 +18,28 @@ function Media() {
   return (
     <div className='media'>
       <div className='media-data'>
-        {/* <video  >
-          <source src='one.mp4' type='video/mgitp4'></source>
-        </video> */}
-        <embed src='suffer.mp4' width={"100%"}></embed>
-        {/* <img src='dorm.png' alt='hoto' /> */}
-
+        <embed src='coder.mp4' width={"100%"}></embed>
       </div>
       <div className='medi'>
         <div className='med'>is coding still worth in
-          2024 (s an ex-google</div>
-        <div className='med'><button>Download</button></div>
+          2024 </div>
+        <div classN66ame='med'><button>Download</button></div>
+      </div>
+      <div className='mede'>
+        <a href='https://www.youtube.com/watch?v=wiIR8t8qQsM' target='_blank' >Watch full video here</a>
       </div>
       <div className='media-name'></div>
       <div className='media-button'></div>
     </div>
   )
 }
+function Welcomebox() {
+  return (
+    <div className='welcomebox'>
+      <img src='dorm.png' alt='hoto' />
+      Advertesing! <br />
+
+    </div>
+  )
+};
 export default App;
--- src/header.js
+++ src/header.js
@@ -2,7 +2,9 @@ import './App.css';
 function Header() {
     return (
         <div className='header'>
-            <h1><span id="media">Sunnahweb</span></h1>
+            <div><h1><span id="media">Arewa<span id="site">media!</span></span></h1>
+            </div>
+            <div><img src='menuicon.png' alt='menu' /> </div>
         </div>
     );
 }
